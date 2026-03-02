a49064fade5370e2c3ca00f51e7db9bc0647232a
marcelaobeso <march.obeso@gmail.com>
2026-03-02 14:14:08 +0000

    fix: updated imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
-import Clock from "@/components/Clock";
-import Title from "@/components/Title";
-import Image from "next/image";
 import About from "./about/page";
-import SideNavigation from "@/components/SideNavigation";
 
 export default function Home() {
   return (
